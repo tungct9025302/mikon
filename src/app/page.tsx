148f43a61c8a8a7d02edee76ed510e6f867e73f9
unknown <yesphuocthinh451999@gmail.com>
2024-10-24 00:34:17 +0700

    Update some details

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState } from "react";
 
 import Image from "next/image";
 import icon from "../app/assets/icon.png";
@@ -8,14 +9,24 @@ import { Avatar } from "@material-tailwind/react";
 import Searchbar from "./components/page";
 
 export default function homepage() {
+  const [hover, setHover] = useState();
+
   return (
-    <div className="flex flex-row px-10 text-black font-bold text-[1.2em] items-center justify-between h-[70px] bg-[#fdfcfc]">
+    <div className="flex flex-row px-10  font-bold text-[1.2em] items-center justify-between h-[70px] bg-[#fdfcfc]">
       <div className="flex flex-row space-x-16 items-center">
         <div>
           <Image src={icon} width={200} height={50} alt=""></Image>
         </div>
-        <div className="hover:text-white">Home</div>
-        <div>Games</div>
+        <div className="hover:text-black  text-zinc-400">
+          <div className="pb-2">Home</div>
+
+          <div className="w-[50%] max-w-[20px] border-b-4 border-sky-600 m-auto "></div>
+        </div>
+        <div className="hover:text-black text-zinc-400">
+          <div className="pb-2">Categories</div>
+
+          <div className="w-[50%] max-w-[20px]  border-b-4 border-sky-600 m-auto"></div>
+        </div>
       </div>
 
       <div className="w-[26vw] items-center">
